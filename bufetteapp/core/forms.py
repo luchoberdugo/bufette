19841ff42508884a955762d656df77383694d63b
Eder Lara T <ederlatru@gmail.com>
2024-02-23 21:56:24 -0500

    Inicio Modelo DB

--- bufetteapp/core/forms.py
+++ bufetteapp/core/forms.py
@@ -1,1 +1,7 @@
-from django.forms import forms
+from django import forms
+
+# creamos la clase de formulario:
+class ContactoForm(forms.Form):
+    nombre = forms.CharField(required=True, widget=forms.TextInput(attrs={'class':'form-control'}))
+    correo = forms.EmailField(required=True, widget=forms.EmailInput(attrs={'class':'form-control'}))
+    mensaje= forms.CharField(required=True, widget=forms.Textarea(attrs={'class':'form-control', 'rows':'4'}))
--- bufetteapp/core/views.py
+++ bufetteapp/core/views.py
@@ -1,8 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
 # vistas basadas en clases:
-from django.views.generic import ListView, TemplateView
+from django.views.generic import ListView, TemplateView, View
 # Importamos el modelo de datos de core:
 from .models import Contacto, Nosotros, Servicios
+# Envio de correos:
+from django.core.mail import send_mail
+# Importamos el forulario:
+from .forms import ContactoForm
 
 # Create your views here.
 class IndexPageView(TemplateView):
@@ -21,12 +25,26 @@ class NosotrosPageView(TemplateView):
     def get(self, request, *args, **kwargs):
         return render(request, self.template_name)    
 
-class ContactoPageView(TemplateView):
+class ContactoPageView(View):
     """ Vista para pagina de Contacto """
     template_name = "core/contacto.html"
 
     def get(self, request, *args, **kwargs):
-        return render(request, self.template_name)
+        form = ContactoForm()
+        return render(request, self.template_name, {'form': form})
+    
+    def post(self, request, *args, **kwars):
+        # Creamos un objeto del formulario y le pasamos los valores
+        form = ContactoForm(request.POST)
+        if form.is_valid():
+            send_mail(
+                'Asunto desde Formulario',
+                form.cleaned_data['mensaje'],
+                form.cleaned_data['email'],
+                ['usuario@mail.com'],
+            )
+            return HttpResponseRedirect('/')
+        return render(request, self.template_name, {'form': form})        
         
 class ServiciosPageView(ListView):
     """ Vista para pagina de Servicios """
