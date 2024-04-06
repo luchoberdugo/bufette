1bb51ccba4af7218beba830f65786341b2f41f3c
Eder Lara T <ederlatru@gmail.com>
2024-04-06 12:35:29 -0500

    Configuracion ini semillas

--- bufetteapp/dane/models.py
+++ bufetteapp/dane/models.py
@@ -7,7 +7,19 @@ from django.db import models
 class Departamento(models.Model):
     name = models.CharField(max_length=255)
 
+    class Meta:
+        verbose_name = 'Departamentos'
+
+    def __str__(self) -> str:
+        return f'{self.name}'
+
 
 class Ciudad(models.Model):
     name = models.CharField(max_length=255)
     departamento = models.ForeignKey(Departamento, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name_plural = 'Ciudades'
+
+    def __str__(self):
+        return f'{self.name}'
---
+++ bufetteapp/seed.py
@@ -0,0 +1,19 @@
+import os
+import django
+from dotenv import load_dotenv, find_dotenv
+
+load_dotenv(find_dotenv())
+os.environ["DJANGO_SETTINGS_MODULE"] = "src.settings"
+django.setup()
+
+# Importamos las clases que incluiremos en las semillas:
+from seeds.datadepto import DepartamentoSeed
+
+
+def execute_seed():
+    departamento_seed = DepartamentoSeed()
+    if departamento_seed.should_run():
+        departamento_seed.run()
+
+
+execute_seed()
