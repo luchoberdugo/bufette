3533989feb82f47274a5642149481780e1b42a3b
Eder Lara T <ederlatru@gmail.com>
2024-04-19 23:12:37 -0500

    Configuración inicial dashboard

--- bufetteapp/registration/urls.py
+++ bufetteapp/registration/urls.py
@@ -1,1 +1,6 @@
 from django.urls import path
+from .views import DashboardUserView
+
+urlpatterns = [
+    path('inicio/', DashboardUserView.as_view(), name= 'dashboard'),
+]
--- bufetteapp/src/urls.py
+++ bufetteapp/src/urls.py
@@ -22,5 +22,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
     path('accounts/', include('django.contrib.auth.urls')),         # Con esta url configuramos el login
+    path('users/', include('registration.urls')),
 ]
 
