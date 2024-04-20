58c26cffe0a7748b1d30edba44a00697207a408a
Eder Lara T <ederlatru@gmail.com>
2024-04-19 20:55:58 -0500

    Update Login

--- bufetteapp/src/urls.py
+++ bufetteapp/src/urls.py
@@ -20,5 +20,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('core.urls'))
+    path('', include('core.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),         # Con esta url configuramos el login
 ]
+
